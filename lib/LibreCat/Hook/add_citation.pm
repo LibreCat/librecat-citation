625431984839d671b085372335de5648d2d69a23
Vitali Peil <vitali.peil@uni-bielefeld.de>
2020-02-24 11:15:48 +0100

    initial commit: moving sources from main repo

---
+++ lib/LibreCat/Hook/add_citation.pm
@@ -0,0 +1,29 @@
+package LibreCat::Hook::add_citation;
+
+=pod
+
+=head1 NAME
+
+LibreCat::Hook::add_citation - add a 'citation' field calculated from the data
+
+=cut
+
+use Catmandu::Sane;
+use LibreCat::Citation;
+use Moo;
+
+has citation => (is => 'lazy');
+
+sub _build_citation {
+    LibreCat::Citation->new();
+}
+
+sub fix {
+    my ($self, $data) = @_;
+
+    my $citation = $self->citation->add($data);
+
+    return $data;
+}
+
+1;
